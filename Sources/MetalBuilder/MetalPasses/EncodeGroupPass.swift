5654cbcabacc30450365c43fdddeabc3dfa105ed
Roman Gaditskiy <51689121+gadirom@users.noreply.github.com>
2022-10-08 14:07:45 +0300

    active in EncodeGroup

--- Sources/MetalBuilder/MetalPasses/EncodeGroupPass.swift
+++ Sources/MetalBuilder/MetalPasses/EncodeGroupPass.swift
@@ -8,10 +8,12 @@ class EncodeGroupPass: MetalPass{
     var libraryContainer: LibraryContainer?
     let passes: [MetalPass]
     let repeating: Binding<Int>
+    let active: Binding<Bool>
     
-    init(_ passes: [MetalPass], repeating: Binding<Int>){
+    init(_ passes: [MetalPass], repeating: Binding<Int>, active: Binding<Bool>){
         self.passes = passes
         self.repeating = repeating
+        self.active = active
     }
     func setup(device: MTLDevice) throws{
         for pass in passes {
@@ -19,7 +21,8 @@ class EncodeGroupPass: MetalPass{
         }
     }
     func encode(_ commandBuffer: MTLCommandBuffer,_ drawable: CAMetalDrawable?) throws {
-        for _ in 0..<repeating.wrappedValue{
+        let repeating = repeating.wrappedValue * (active.wrappedValue ? 1:0)
+        for _ in 0..<repeating{
             for pass in passes {
                 try pass.encode(commandBuffer, drawable)
             }
