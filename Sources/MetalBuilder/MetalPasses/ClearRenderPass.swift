e4a365efda301808e4539934dd4483c98871b611
Roman Gaditskiy <51689121+gadirom@users.noreply.github.com>
2022-11-20 11:19:24 +0300

    Update ClearRenderPass.swift

--- Sources/MetalBuilder/MetalPasses/ClearRenderPass.swift
+++ Sources/MetalBuilder/MetalPasses/ClearRenderPass.swift
@@ -9,6 +9,7 @@ class ClearRenderPass: MetalPass{
         let commandBuffer = passInfo.getCommandBuffer()
         let renderPassDescriptor =
         passInfo.renderPassDescriptor
+        renderPassDescriptor.colorAttachments[0].clearColor = .init(red: 0, green: 0, blue: 0, alpha: 0)
         let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
         renderEncoder.endEncoding()
     }
