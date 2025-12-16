00fdca0c12ce56ed507ca2f80385dcdbdc8c2a5b
Roman Gaditskiy <51689121+gadirom@users.noreply.github.com>
2025-12-16 06:47:45 +0100

    Update Math.swift

--- Sources/MetalBuilder/Helpers/Math.swift
+++ Sources/MetalBuilder/Helpers/Math.swift
@@ -11,6 +11,10 @@ public extension simd_float3x3{
     static var identity: Self{
         .init(1)
     }
+    func transformed2D(_ coord: simd_float2)->simd_float2{
+        let coord1: simd_float3 = [coord.x, coord.y, 1]*self
+        return [coord1.x, coord1.y]
+    }
 }
 
 public extension simd_float4x4{
