273068f6906b1d07b3a8c37ae63e769ac9d372f5
Roman Gaditskiy <51689121+gadirom@users.noreply.github.com>
2025-12-16 02:46:00 +0100

    Create Math.swift

---
+++ Sources/MetalBuilder/Helpers/Math.swift
@@ -0,0 +1,20 @@
+//
+//  Math.swift
+//  MetalBuilder
+//
+//  Created by Roman Gaditskiy on 14. 12. 2025..
+//
+
+import MetalKit
+
+public extension simd_float3x3{
+    static var identity: Self{
+        .init(1)
+    }
+}
+
+public extension simd_float4x4{
+    static var identity: Self{
+        .init(1)
+    }
+}
